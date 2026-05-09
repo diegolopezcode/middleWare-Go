535bec56b1524551514e3ae3365bb161deff899c
Diego Lopez <diegolopez.coder@gmail.com>
2026-05-09 11:43:29 -0500

    Add authentication request and response structures for user login and registration

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"task-api/internal/config"
+	"task-api/internal/domain/models"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -20,4 +21,14 @@ type RegisterRequest struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required, min=8"`
 	Name     string `json:"name" validate:"required,min=2"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type AuthResponse struct {
+	Token string              `json:"token"`
+	User  models.UserResponse `json:"user"`
 }
